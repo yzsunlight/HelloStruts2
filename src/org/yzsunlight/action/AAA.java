63f612719abcf2c4b91b2853b29108aa30f7b0f0
yangz <yangz@freshpower.cn>
2016-04-21 09:39:53 +0800

    33333

--- src/org/yzsunlight/action/AAA.java
+++ src/org/yzsunlight/action/AAA.java
@@ -27,6 +27,7 @@ public class AAA extends ComponentTagSupport{
 		System.out.println("11111111111");
 		System.out.println("22222222222");
 		System.out.println("33333333333");
+		System.out.println("44444444444");
 		
 		return null;
 	}
